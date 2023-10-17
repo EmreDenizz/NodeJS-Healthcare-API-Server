79bc3faa28868c1bc422a430a72d5cf5e2c10c90
Muindo N. Gituku <ngeenegituku@gmail.com>
2023-10-17 12:43:00 -0400

    Adding a new Patient

--- app.js
+++ app.js
@@ -57,6 +57,55 @@ server.get('/patients', function (req, res, next) {
         });
 })
 
+//adding a new patient
+server.post('/patients', function (req, res, next) {
+    console.log('POST /patients params=>' + JSON.stringify(req.params));
+    console.log('POST /patients body=>' + JSON.stringify(req.body));
+
+    //validation using the patients model structure
+    if (req.body.first_name === undefined) {
+        return next(new errors.BadRequestError('You did not provide the First Name!!'))
+    }
+    if (req.body.last_name === undefined) {
+        return next(new errors.BadRequestError('You did not provide the Last Name!!'))
+    }
+    if (req.body.address === undefined) {
+        return next(new errors.BadRequestError('You did not provide the Home Address!!'))
+    }
+    if (req.body.date_of_birth === undefined) {
+        return next(new errors.BadRequestError('You did not provide the Date of Birth!!'))
+    }
+    if (req.body.department === undefined) {
+        return next(new errors.BadRequestError('You did not provide the Department visited!!'))
+    }
+    if (req.body.doctor === undefined) {
+        return next(new errors.BadRequestError('You did not provide the name of the Doctor attending!!'))
+    }
+
+    //create a new patient model from the values entered in the body
+    let newPatient = new PatientsModel({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        address: req.body.address,
+        date_of_birth: req.body.date_of_birth,
+        department: req.body.department,
+        doctor: req.body.doctor
+    })
+
+    //save the new patient to the database
+    newPatient.save()
+        .then((patient) =>{
+            console.log("Saved Patient: " + patient);
+            // Send the patient if no issues
+            res.send(201, patient);
+            return next();
+        })
+        .catch((error)=>{
+            console.log("Error Saving the Patient: " + error);
+            return next(new Error(JSON.stringify(error.errors)));
+    });
+})
+
 // Get a single patient by id
 server.get('/patients/:id', function (req, res, next) {
     console.log('GET /patients/:id params=>' + JSON.stringify(req.params));
